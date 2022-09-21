fbfac45b5f89790297e9cda1480f37c7af97aa02
s.christodoulou <s.christodoulou@pmmretail.com>
2022-09-22 00:54:37 +0300

    shared service

---
+++ src/shared/shared.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+
+  id!:number;
+  constructor() { }
+
+  setId(data: number){
+    this.id=data;
+  }
+
+  getId(){
+    return this.id;
+  }
+}
