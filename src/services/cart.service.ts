2507afcc21fae4fe917e789f5bec7b2407b1e71d
s.christodoulou <s.christodoulou@pmmretail.com>
2022-09-22 00:53:50 +0300

    cart service

---
+++ src/services/cart.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  private cart:any;
+
+  addToCart():void{
+    let orderItem = this.cart
+  }
+
+  constructor() { }
+}
